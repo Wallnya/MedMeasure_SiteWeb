21b69f2c26d3a65efdc7bc4f83d1a1983097d5ff
Adrien-C <56512625+Adrien-C@users.noreply.github.com>
2019-11-30 18:03:23 +0100

    base de données complète

--- medmeasure.sql
+++ medmeasure.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  lun. 04 nov. 2019 à 14:11
--- Version du serveur :  10.1.38-MariaDB
--- Version de PHP :  7.3.2
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 30 nov. 2019 à 16:59
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,11 +28,13 @@ SET time_zone = "+00:00";
 -- Structure de la table `connexion`
 --
 
-CREATE TABLE `connexion` (
+DROP TABLE IF EXISTS `connexion`;
+CREATE TABLE IF NOT EXISTS `connexion` (
   `email` varchar(50) NOT NULL,
   `mdp` varchar(50) NOT NULL,
   `type` varchar(50) NOT NULL,
-  `idUtilisateur` varchar(50) NOT NULL
+  `idUtilisateur` varchar(50) NOT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,7 +44,45 @@ CREATE TABLE `connexion` (
 INSERT INTO `connexion` (`email`, `mdp`, `type`, `idUtilisateur`) VALUES
 ('admin@admin.fr', 'admin', 'Administrateur', '1'),
 ('test2@test.fr', 'unmotdepasse', 'Gestionnaire', '3'),
-('test@test.fr', 'mdp', 'Utilisateur', '2');
+('test3@test.fr', 'mdp', 'Pilote', '4'),
+('test4@test.fr', '05a671c66aefea124cc08b76ea6d30bb', 'Pilote', '7'),
+('test5@test.fr', 'test5', 'Pilote', '5'),
+('test6@test.fr', 'test6', 'Pilote', '6'),
+('test@test.fr', 'mdp', 'Pilote', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `testcomplet`
+--
+
+DROP TABLE IF EXISTS `testcomplet`;
+CREATE TABLE IF NOT EXISTS `testcomplet` (
+  `idTestComplet` int(11) NOT NULL AUTO_INCREMENT,
+  `idUtilisateur` int(11) NOT NULL,
+  `Numero_Test` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Frequence` int(11) NOT NULL,
+  `PerceptionAuditive` int(11) NOT NULL,
+  `StimulusVisuel` int(11) NOT NULL,
+  `TemperaturePeau` int(11) NOT NULL,
+  `RecoTonalite` int(11) NOT NULL,
+  `id_boitier` int(11) NOT NULL,
+  `ScoreTotal` int(11) NOT NULL,
+  PRIMARY KEY (`idTestComplet`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `testcomplet`
+--
+
+INSERT INTO `testcomplet` (`idTestComplet`, `idUtilisateur`, `Numero_Test`, `Date`, `Frequence`, `PerceptionAuditive`, `StimulusVisuel`, `TemperaturePeau`, `RecoTonalite`, `id_boitier`, `ScoreTotal`) VALUES
+(1, 1, 0, '2019-11-03', 89, 30, 300, 34, 2000, 0, 70),
+(2, 2, 0, '2019-11-18', 67, 37, 224, 21, 1570, 0, 88),
+(3, 3, 0, '2019-11-14', 113, 22, 509, 37, 870, 0, 54),
+(4, 4, 0, '2019-11-01', 89, 37, 440, 22, 3620, 0, 74),
+(5, 5, 0, '2019-11-25', 78, 34, 415, 25, 1200, 0, 66),
+(6, 6, 0, '2019-11-29', 102, 20, 769, 29, 740, 0, 47);
 
 -- --------------------------------------------------------
 
@@ -50,24 +90,69 @@ INSERT INTO `connexion` (`email`, `mdp`, `type`, `idUtilisateur`) VALUES
 -- Structure de la table `testpartiel`
 --
 
-CREATE TABLE `testpartiel` (
-  `idTestPartiel` int(11) NOT NULL,
+DROP TABLE IF EXISTS `testpartiel`;
+CREATE TABLE IF NOT EXISTS `testpartiel` (
+  `idTestPartiel` int(11) NOT NULL AUTO_INCREMENT,
   `idUtilisateur` int(11) NOT NULL,
+  `Numero_Test` int(11) NOT NULL,
+  `Date` date NOT NULL,
   `Frequence` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `numero_test` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `PerceptionAuditive` int(11) NOT NULL,
+  `StimulusVisuel` int(11) NOT NULL,
+  `id_boitier` int(11) NOT NULL,
+  `ScoreTotal` int(11) NOT NULL,
+  PRIMARY KEY (`idTestPartiel`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `testpartiel`
 --
 
-INSERT INTO `testpartiel` (`idTestPartiel`, `idUtilisateur`, `Frequence`, `date`, `numero_test`) VALUES
-(1, 1, 10, '2019-11-12', 0),
-(2, 1, 165, '2019-08-20', 1),
-(3, 2, 180, '2019-11-20', 0),
-(4, 3, 80, '2019-04-07', 0),
-(5, 2, 40, '0000-00-00', 1);
+INSERT INTO `testpartiel` (`idTestPartiel`, `idUtilisateur`, `Numero_Test`, `Date`, `Frequence`, `PerceptionAuditive`, `StimulusVisuel`, `id_boitier`, `ScoreTotal`) VALUES
+(1, 1, 0, '2019-10-14', 102, 36, 270, 0, 67),
+(2, 1, 1, '2019-09-17', 56, 22, 330, 0, 78),
+(3, 3, 0, '2019-10-09', 76, 33, 240, 0, 86),
+(4, 7, 0, '2019-09-12', 87, 36, 890, 0, 38),
+(5, 7, 1, '2019-12-12', 84, 28, 570, 0, 55),
+(6, 3, 1, '2019-11-21', 113, 39, 120, 0, 57),
+(7, 5, 0, '2019-08-19', 66, 42, 450, 0, 60),
+(8, 5, 1, '2019-07-03', 90, 21, 670, 0, 58),
+(9, 6, 0, '2019-07-01', 68, 35, 350, 0, 82),
+(10, 6, 1, '2019-06-03', 124, 21, 210, 0, 77),
+(11, 4, 0, '2019-07-26', 90, 33, 150, 0, 92);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `idTicket` int(11) NOT NULL AUTO_INCREMENT,
+  `intitule` varchar(50) NOT NULL,
+  `reponse` varchar(250) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  `dateEnvoi` date NOT NULL,
+  `contenu` varchar(250) NOT NULL,
+  PRIMARY KEY (`idTicket`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `ticket`
+--
+
+INSERT INTO `ticket` (`idTicket`, `intitule`, `reponse`, `idUtilisateur`, `statut`, `dateEnvoi`, `contenu`) VALUES
+(1, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 1, 0, '2019-11-05', '\r\n\r\n\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n'),
+(2, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 4, 0, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(3, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 4, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(4, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 6, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(5, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 6, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(6, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 2, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(7, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 1, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(8, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 5, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r'),
+(9, '  Regum tuis ponere tuis nec.', '\r\n\r\nSed tamen haec cum ita tutius observentur, quidam vigore artuum inminuto rogati ad nuptias ubi aurum dextris manibus cavatis offertur, inpigre vel usque Spoletium pergunt. haec nobilium sunt instituta.\r\n\r\n', 7, 1, '2019-11-05', 'Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.\r');
 
 -- --------------------------------------------------------
 
@@ -75,8 +160,9 @@ INSERT INTO `testpartiel` (`idTestPartiel`, `idUtilisateur`, `Frequence`, `date`
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `idUtilisateur` int(11) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(256) DEFAULT NULL,
   `prenom` varchar(256) DEFAULT NULL,
   `DN` date NOT NULL,
@@ -84,8 +170,9 @@ CREATE TABLE `utilisateur` (
   `AdresseVoie` varchar(50) NOT NULL,
   `AdresseVille` varchar(50) NOT NULL,
   `AdresseCP` int(11) NOT NULL,
-  `Tel` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Tel` varchar(50) NOT NULL,
+  PRIMARY KEY (`idUtilisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
@@ -97,39 +184,8 @@ INSERT INTO `utilisateur` (`idUtilisateur`, `nom`, `prenom`, `DN`, `Sexe`, `Adre
 (3, 'My-linh', 'My-linh', '1968-08-22', 'Femme', '96 rue charles henrie', 'Clamart', 98750, '0102030405'),
 (4, 'Peter', 'Peter', '1950-12-01', 'Homme', '56 avenue des moutons', 'Meudon', 45607, '0804060512'),
 (5, 'Adrien', 'Adrien', '1980-04-15', 'Homme', '56 boulevard henrie', 'fdsdfsfd', 45144, '0605040203'),
-(6, 'Ludivine', 'Ludivine', '1970-03-26', 'Femme', '33 rue des champs', 'Sèvres', 92310, '0508091245');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `connexion`
---
-ALTER TABLE `connexion`
-  ADD PRIMARY KEY (`email`);
-
---
--- Index pour la table `testpartiel`
---
-ALTER TABLE `testpartiel`
-  ADD PRIMARY KEY (`idTestPartiel`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`idUtilisateur`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+(6, 'Ludivine', 'Ludivine', '1970-03-26', 'Femme', '33 rue des champs', 'Sèvres', 92310, '0508091245'),
+(7, 'JOHN', 'Doe', '2019-11-28', 'Homme', '11 rue des sablons', 'Paris', 33700, '0650493912');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
