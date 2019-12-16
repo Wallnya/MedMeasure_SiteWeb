ef76d646765375439e50aa20945d5149fbeded77
Wallnya <34969114+Wallnya@users.noreply.github.com>
2019-12-16 10:47:14 +0100

    JQuery onchange

--- gestionnaire2.php
+++ gestionnaire2.php
@@ -17,19 +17,19 @@
     <img src="images/gestion.png" alt="Image pour la page resultat">
   </div>
 </header>
-
+<body>
 <?php
 		$serveur = "localhost";
 		$login = "root";
     $mdp = "";
-    
+
 		#connexion au serveur de la base de données
 		$connexion = mysqli_connect($serveur,$login,$mdp)
     or die ("Connexion au serveur $serveur impossible pour $login");
-    
+
 		#nom de la base de données
     $bd = "medmeasure";
-    
+
 		#connexion à la base de données
 		mysqli_select_db($connexion,$bd)
     or die ("Impossible d'accéder à la base de données");
@@ -40,25 +40,6 @@
 <div class="search-dropdown">
 <input type="text" name="search" placeholder="Recherche d'utilisateur">
 <button type="submit" class="Rechercher">Rechercher</button>
-
-<script>
-
-function click_me(element)
- {
-      try
-   {//pour ie
-  document.getElementById(element).click();
-   }catch(e)
-   {//pour ff
-    var evt = document.createEvent("MouseEvents"); // créer un événement souris
-    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);  // intiailser l'évennement déja crée par un click
-    var cb = document.getElementById(element); // pointe sur l'élement
-    cb.dispatchEvent(evt);  // envoyer l'événement vers l'élement
-   }
- }
-
-</script>
-
 <?php
   if (isset($_GET['search']) && !empty($_GET['search']) && $_GET['search'] !== '  ') {
   $search = $_GET['search'];
@@ -68,16 +49,23 @@ function click_me(element)
       $output = 'Pas de résultat pour <b>"'.$search.'"</b>';
   }
   else {
-      echo "<select onchange=\"click_me('element')\">";
+      echo "<select onchange=\"change(this.value)\">";
       while ($row = mysqli_fetch_array($query)) {
-      echo "<option value=\"nom\">".$row['nom'].' '.$row['prenom']."</option>";
-      }
+        ?>
+        <option id=<?=$row['nom']?> value=<?=$row['nom']?>><?=$row['nom']?><?=$row['prenom']?></option>;
+  <?php    }
       echo "</select>";
   }
   }
 
 ?>
-<body>
 </body>
+<script>
+
+function change(element)
+{
+  alert(element);
+}
+ </script>
 
 </html>
