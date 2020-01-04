fab2d0262b34d871885a233da51652e630080f85
Adrien-C <56512625+Adrien-C@users.noreply.github.com>
2020-01-04 16:32:24 +0100

    Ajout gestion page ticket

--- index.php
+++ index.php
@@ -9,45 +9,23 @@ if (isset($_GET['page'])) {
     if (isset($_POST['modifier'])){
       if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
         if (isset($_POST["Type"])){
-          $type=htmlspecialchars($_POST['Type']);
-          $id=htmlspecialchars($_POST['idUtilisateur']);
-          modifyUser($type,$id);
+          modifyUser($_POST["Type"],$_POST['idUtilisateur']);
         }
       }
     }
     else if (isset($_POST['supprimer'])){
       if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
-        $id=htmlspecialchars($_POST['idUtilisateur']);
-        deleteUser($id);
-      }
-    }
-    else if (isset($_POST['bannir'])){
-      if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
-        $id=htmlspecialchars($_POST['idUtilisateur']);
-        banUser($id);
-      }
-    }
-    if (isset($_POST['modifierValide'])){
-      if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
-        if (isset($_POST["Valide"])){
-          $id=htmlspecialchars($_POST['idUtilisateur']);
-          $valide=htmlspecialchars($_POST['Valide']);
-          modifyValide($valide,$id);
-        }
+        deleteUser($_POST['idUtilisateur']);
       }
     }
     else if(isset($_POST['supprimerTestPartiel'])){
       if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
-        $id=htmlspecialchars($_POST['idUtilisateur']);
-        $idtest=htmlspecialchars($_POST['idtest']);
-        deleteTestPartiel($id,$idtest);
+        deleteTestPartiel($_POST['idUtilisateur'],$_POST['idtest']);
       }
     }
     else if(isset($_POST['supprimerTestComplet'])){
       if (isset($_POST['idUtilisateur']) && $_POST['idUtilisateur'] > 0) {
-        $id=htmlspecialchars($_POST['idUtilisateur']);
-        $idtest=htmlspecialchars($_POST['idtest']);
-        deleteTestComplet($id,$idtest);
+        deleteTestComplet($_POST['idUtilisateur'],$_POST['idtest']);
       }
     }
     else {
@@ -57,9 +35,7 @@ if (isset($_GET['page'])) {
   else if ($_GET['page']=='admin_ticket'){
     if (isset($_POST['modifier'])){
       if (isset($_POST['idTicket']) && $_POST['idTicket'] > 0) {
-        $statut=htmlspecialchars($_POST['idUtilisateur']);
-        $idTicket=htmlspecialchars($_POST['idtest']);
-        modifyTicket($statut,$idTicket);
+        modifyTicket($_POST["Statut"],$_POST['idTicket']);
       }
     }
     else{
@@ -70,23 +46,18 @@ if (isset($_GET['page'])) {
     if (isset($_POST['modifier'])){
       if (isset($_POST['idFAQ']) && $_POST['idFAQ'] > 0) {
         if (isset($_POST["message"])){
-          $message=htmlspecialchars($_POST['message']);
-          $idFAQ=htmlspecialchars($_POST['idFAQ']);
-          modifyFAQ($message,$idFAQ);
+          modifyFAQ($_POST["message"],$_POST['idFAQ']);
         }
       }
     }
     else if (isset($_POST['supprimer'])){
       if (isset($_POST['idFAQ']) && $_POST['idFAQ'] > 0) {
-        $idFAQ=htmlspecialchars($_POST['idFAQ']);
-        deleteFAQ($idFAQ);
+        deleteFAQ($_POST['idFAQ']);
       }
     }
     else if (isset($_POST['enregistrerFAQ'])){
       if (isset($_POST["question"]) && isset($_POST["reponse"])){
-        $question=htmlspecialchars($_POST['question']);
-        $reponse=htmlspecialchars($_POST['reponse']);
-        addFAQ($question,$reponse);
+        addFAQ($_POST['question'],$_POST['reponse']);
       }
     }
     else {
@@ -100,45 +71,31 @@ if (isset($_GET['page'])) {
     if (isset($_POST["boutonEnregistrer"])){
       if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['dn']) && isset($_POST['sexe']) &&
       isset($_POST['adresse']) && isset($_POST['ville']) && isset($_POST['cp']) && isset($_POST['tel'])){
-        $id=htmlspecialchars($_SESSION['id']);
-        $nom=htmlspecialchars($_POST['nom']);
-        $prenom=htmlspecialchars($_POST['prenom']);
-        $dn=htmlspecialchars($_POST['dn']);
-        $sexe=htmlspecialchars($_POST['sexe']);
-        $adresse=htmlspecialchars($_POST['adresse']);
-        $ville=htmlspecialchars($_POST['ville']);
-        $cp=htmlspecialchars($_POST['cp']);
-        $tel=htmlspecialchars($_POST['tel']);
-        modif_profil($id,$nom,$prenom,$dn,$sexe,$adresse,$ville,$cp,$tel);
+        modif_profil($_SESSION['id'],$_POST['nom'],$_POST['prenom'],$_POST['dn'],$_POST['sexe'],$_POST['adresse'],$_POST['ville'],$_POST['cp'],$_POST['tel']);
       }
     }
     else if (isset($_POST["modifProfil"])){
-      $id=htmlspecialchars($_SESSION['id']);
-      page_modif_profil($id);
+      page_modif_profil($_SESSION['id']);
     }
     else if (isset($_POST['test'])){
-      $id=htmlspecialchars($_SESSION['id']);
-      page_test($id);
+      page_test($_SESSION['id']);
     }
     else if(isset($_POST['Dernierresultat'])){
-      $id=htmlspecialchars($_SESSION['id']);
-      page_dernierresultat($id);
+      page_dernierresultat($_SESSION['id']);
     }
     else if(isset($_POST['histo'])){
-      $id=htmlspecialchars($_SESSION['id']);
-      page_historique($id);
+      page_historique($_SESSION['id']);
     }
-    else if(isset($_POST['detail'])){
-      if (isset($_POST['type']) && isset($_POST['idTest'])){
-        $id=htmlspecialchars($_SESSION['id']);
-        $type=htmlspecialchars($_POST['type']);
-        $idTest=htmlspecialchars($_POST['idTest']);
-        page_resultat($type,$idTest,$id);
+    /*else if(isset($_POST['ticket'])){
+      page_ticket($_SESSION['id']);
+    }
+    else if (isset($_POST['EnvoyerTicket'])){
+      if (isset($_POST["intitule"]) && isset($_POST["contenu"])){
+        addTicket($_POST['intitule'],$_POST['contenu']);
       }
-    }
+    }*/
     else{
-      $id=htmlspecialchars($_SESSION['id']);
-      page_user($id);
+      page_user($_SESSION['id']);
     }
   }
   /********************************/
@@ -154,25 +111,24 @@ if (isset($_GET['page'])) {
       page_faq();
   }
   /********************************/
+  /*            TICKET            */
+  /********************************/
+  else if ($_GET['page']=='ticket'){
+    page_ticket();
+     if (isset($_POST['EnvoyerTicket'])){
+      if (isset($_POST["intitule"]) && isset($_POST["contenu"])){
+        addTicket($_POST['intitule'],$_POST['contenu']);
+      }
+    }
+  }
+  /********************************/
   /*          INSCRIPTION         */
   /********************************/
   else   if ($_GET['page'] == 'inscription'){
     if (isset($_POST["boutonInscription"])){
       if (isset($_POST['email']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['dn']) && isset($_POST['sexe']) &&
       isset($_POST['adresse']) && isset($_POST['ville']) && isset($_POST['cp']) && isset($_POST['tel']) && isset($_POST['mdp'])){
-        $id=htmlspecialchars($_SESSION['id']);
-        $nom=htmlspecialchars($_POST['nom']);
-        $prenom=htmlspecialchars($_POST['prenom']);
-        $dn=htmlspecialchars($_POST['dn']);
-        $sexe=htmlspecialchars($_POST['sexe']);
-        $adresse=htmlspecialchars($_POST['adresse']);
-        $ville=htmlspecialchars($_POST['ville']);
-        $cp=htmlspecialchars($_POST['cp']);
-        $tel=htmlspecialchars($_POST['tel']);
-        $email=htmlspecialchars($_POST['email']);
-        $mdp=htmlspecialchars($_POST['mdp']);
-        $case=htmlspecialchars($_POST['case']);
-        checkUser($email,$nom,$prenom,$dn,$sexe,$adresse,$ville,$cp,$tel,$mdp,$case);
+        checkUser($_POST['email'],$_POST['nom'],$_POST['prenom'],$_POST['dn'],$_POST['sexe'],$_POST['adresse'],$_POST['ville'],$_POST['cp'],$_POST['tel'],$_POST['mdp']);
       }
       deconnexion();
       accueil();
@@ -188,6 +144,8 @@ if (isset($_GET['page'])) {
       cgu();
   }
 }
+
+
 /********************************/
 /*          DECONNEXION         */
 /********************************/
@@ -201,9 +159,7 @@ else if (isset($_GET['deco'])) {
 /********************************/
 else if (isset($_POST['connexion'])){
   if (isset($_POST['mail']) && isset($_POST['mdp'])){
-    $email=htmlspecialchars($_POST['mail']);
-    $mdp=htmlspecialchars($_POST['mdp']);
-    connexion($email,$mdp);
+    connexion($_POST['mail'],$_POST['mdp']);
   }
   accueil();
 }
