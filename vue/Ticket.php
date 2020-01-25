0aef7ae9054db25935c5c042dc73e7ad46e699e4
Adrien-C <56512625+Adrien-C@users.noreply.github.com>
2020-01-25 15:48:48 +0100

    ajout affichage historique tickets

--- vue/Ticket.php
+++ vue/Ticket.php
@@ -2,23 +2,37 @@
 <html>
 <?php
 if ($_SESSION["type"]=="Pilote"){
+  if (isset($_SESSION['lang'])){
+    if($_SESSION['lang'] == "en")
+        include "langues/en.inc";
+    else if ($_SESSION['lang'] == "fr"){
+      include "langues/fr.inc";
+    }
+  }
+  else{
+    include "langues/en.inc";
+  }
   ?>
   <!DOCTYPE html>
   <html>
   <head>
     <meta charset="utf-8" />
     <title>MedMeasure</title>
-    <link rel="stylesheet" href="css/DesignTicket.css" />
+    <link rel="stylesheet" href="css/css_ticketUser.css" />
+    <script type="text/javascript" src="js/deconnexionFacebook.js"></script>
+
   </head>
 
   <header>
   <div class="barre_navigation">
     <img src="images/MedMeasure.png" alt="logo de MedMeasure">
-    <a href="index.php?page=user">Accueil</a>
-    <a href="index.php?page=faq">FAQ</a>
-    <a href="index.php?deco=deconnexion">Déconnexion</a>
-    <button class="test">FR</button>
-    <button class="test">EN</button>
+    <a href="index.php?page=user"><?php echo _ACCUEIL?></a>
+    <a href="index.php?page=faq"><?php echo _BOUTONSFAQ?></a>
+    <a style="cursor:pointer" onclick="deconnexionFB();"><?php echo _DECONNEXION?></a>
+    <form method="POST" action="index.php?page=user">
+      <button type="submit" class="test" name="FR">FR</button>
+      <button type="submit" class="test" name="EN">EN</button>
+    </form>
   </div>
   <div class="texte">
     <img src="images/question.jpg" alt="Image pour la FAQ">
@@ -27,15 +41,54 @@ if ($_SESSION["type"]=="Pilote"){
 
 <body>
 
-<form method="POST" action="index.php?page=ticket">
-        <textarea name="intitule" placeholder="Quel est l'objet de votre question ?"></textarea>
-        <textarea name="contenu" placeholder="Merci de détailler votre question." style="height:200px"></textarea>
+<form class="formulaireTicket" method="POST" action="index.php?page=ticket">
+        <textarea name="intitule" placeholder="<?php echo _QUESTIONOBJECT?>"></textarea>
+        <textarea name="contenu" placeholder="<?php echo _DETAILQUESTION?>" style="height:200px"></textarea>
 
-        <input type="submit" name="EnvoyerTicket" value="Envoyer">
+        <input type="submit" name="EnvoyerTicket" value="<?php echo _ENVOYER?>">
 </form>
 
-</body>
 
+  <div class="container-fluid">
+  <p><?php echo _RECAPTICKETS; ?></p>
+    <center>
+      <table cellpadding='10' cellpacing='9'>
+        <tr class="entete">
+          <td><?php echo _DATETICKET; ?></td>
+          <td><?php echo _INTITULE; ?></td>
+          <td class="test"><?php echo _CONTENU; ?></td>
+        </tr>
+      <?php
+      while ($data = $ticket->fetch())
+      {
+      ?>
+      <tr>
+          <td>
+            <?= htmlspecialchars($data['dateEnvoi']) ?>
+          </td>
+          <td>
+            <?= htmlspecialchars($data['intitule']) ?>
+          </td>
+          <td>
+            <?= htmlspecialchars($data['contenu']) ?>
+          </td>
+      </tr>
+      <?php
+      }
+      $ticket->closeCursor();
+      ?>
+      </table>
+    </center>
+  </div>
+        
+
+
+</body>
+<script>
+if ( window.history.replaceState ) {
+window.history.replaceState( null, null, window.location.href );
+}
+</script>
 </html>
 <?php
 }
